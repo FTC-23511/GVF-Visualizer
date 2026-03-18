392251fe29dba77ce5b1f0296e13f6047365257a
Andrew M <102819214+Discovered12345@users.noreply.github.com>
2026-03-17 17:49:00 -0700

    Delete Visualizer-master directory

--- Visualizer-master/src/utils/draw.ts
+++
@@ -1,8 +0,0 @@
-export function getRandomColor() {
-  var letters = "56789ABCD";
-  var color = "#";
-  for (var i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * letters.length)];
-  }
-  return color;
-}
--- Visualizer-master/src/utils/index.ts
+++
@@ -1,8 +0,0 @@
-export * from "./draw";
-export * from "./math";
-export * from "./javaParser";
-
-export const DPI = 96 / 5;
-
-export const titleCase = (str: string) =>
-  `${str[0].toUpperCase()}${str.slice(1).toLowerCase()}`;
