ce2661079347a5eb2f19cf32859df686e393acb1
Andrew M <102819214+Discovered12345@users.noreply.github.com>
2026-03-19 18:40:10 -0700

    test1

--- src/utils/javaGenerator.ts
+++ src/utils/javaGenerator.ts
@@ -3,28 +3,23 @@ import type { Point, Line } from "../types";
 export function generateJavaCode(startPoint: Point, lines: Line[]): string {
     const startPose = `new Pose2d(${startPoint.x.toFixed(3)}, ${startPoint.y.toFixed(3)}, Math.toRadians(${(startPoint.startDeg ?? 0).toFixed(3)}))`;
     
-    let code = `package org.firstinspires.ftc.teamcode.gvf;
+    let code = `package org.firstinspires.ftc.teamcode.opmode.Auto;
 
 import com.seattlesolvers.solverslib.geometry.Pose2d;
-import java.util.ArrayList;
-import java.util.List;
+import org.firstinspires.ftc.teamcode.gvf.Path;
 
 public class Trajectory {
-    public static List<Spline> getPath() {
-        List<Spline> splines = new ArrayList<>();
-        Pose2d lastPose = ${startPose};
-        
-`;
+    public static Path getPath() {
+        return new Path(${startPose})`;
 
-    lines.forEach((line, idx) => {
+    lines.forEach((line) => {
         const endPose = `new Pose2d(${line.endPoint.x.toFixed(3)}, ${line.endPoint.y.toFixed(3)}, Math.toRadians(${(line.endPoint.endDeg ?? 0).toFixed(3)}))`;
-        const splineClass = line.splineClass || (line.endPoint.heading === "linear" ? "LinearSpline" : "TangentialSpline");
+        const method = line.endPoint.heading === "linear" ? "addLinearPoint" : "addPoint";
         
-        code += `        splines.add(new ${splineClass}(lastPose, ${endPose}));\n`;
-        code += `        lastPose = ${endPose};\n\n`;
+        code += `\n                .${method}(${endPose})`;
     });
 
-    code += `        return splines;
+    code += `\n                .setDecel(true);
     }
 }
 `;
