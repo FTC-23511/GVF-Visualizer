0045a00e30ff645a8be30d73711eddf6be577bdc
Andrew M <102819214+Discovered12345@users.noreply.github.com>
2026-03-20 20:42:07 -0700

    splines

--- src/utils/javaGenerator.ts
+++ src/utils/javaGenerator.ts
@@ -2,27 +2,25 @@ import type { Point, Line } from "../types";
 
 export function generateJavaCode(startPoint: Point, lines: Line[]): string {
     const startPose = `new Pose2d(${startPoint.x.toFixed(3)}, ${startPoint.y.toFixed(3)}, Math.toRadians(${(startPoint.startDeg ?? 0).toFixed(3)}))`;
-    
-    let code = `package org.firstinspires.ftc.teamcode.opmode.Auto;
 
-import com.seattlesolvers.solverslib.geometry.Pose2d;
-import org.firstinspires.ftc.teamcode.gvf.Path;
+    let code = `paths.add(new Path(${startPose})`;
 
-public class Trajectory {
-    public static Path getPath() {
-        return new Path(${startPose})`;
-
+    let currentReversed = false;
     lines.forEach((line) => {
-        const endPose = `new Pose2d(${line.endPoint.x.toFixed(3)}, ${line.endPoint.y.toFixed(3)}, Math.toRadians(${(line.endPoint.endDeg ?? 0).toFixed(3)}))`;
+        const endPose = `new Pose2d(${line.endPoint.x.toFixed(3)}, ${line.endPoint.y.toFixed(3)}, Math.toRadians(${(line.endPoint.heading === "constant" ? (line.endPoint.degrees ?? 0) : (line.endPoint.endDeg ?? 0)).toFixed(3)}))`;
         const method = line.endPoint.heading === "linear" ? "addLinearPoint" : "addPoint";
         
+        if (line.endPoint.heading === "tangential") {
+            if (line.endPoint.reverse !== currentReversed) {
+                currentReversed = line.endPoint.reverse;
+                code += `\n                .setReversed(${currentReversed})`;
+            }
+        }
+
         code += `\n                .${method}(${endPose})`;
     });
 
-    code += `\n                .setDecel(true);
-    }
-}
-`;
+    code += `\n                .setDecel(true));`;
 
     return code;
-}
+}   
